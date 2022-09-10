e0ca6e24b1537c4cf55c4a8e46034266b88dff00
anshi06 <araghya6768@gmail.com>
2022-09-10 11:50:03 +0530

    UsersLists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
+import UsersList from './Components/Users/UsersList';
 import AddUser from './Components/Users/AddUser';
 import './App.css';
 
+
+
 function App() {
+  const[usersList, setUsersList] = useState([]);
   return (
     <div className="App">
       <AddUser/>
+      <UsersList users={usersList}/>
       
     </div>
   );
